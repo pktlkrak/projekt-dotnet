57e852790620483d2dd83f6a5467927ae704eac8
pktlkrak <94856631+pktlkrak@users.noreply.github.com>
2026-06-10 18:07:27 +0200

    Make the root page redirect correctly

--- ClinicManager/Views/Index.cshtml.cs
+++ ClinicManager/Views/Index.cshtml.cs
@@ -5,9 +5,19 @@ namespace ClinicManager.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            if (!(User.Identity?.IsAuthenticated ?? false))
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
 
+            if (User.IsInRole("Admin"))
+                return RedirectToPage("/Admin/Index");
+            if (User.IsInRole("Doctor"))
+                return RedirectToPage("/Doctor/Index");
+            if (User.IsInRole("RegistrationWorker"))
+                return RedirectToPage("/Registrar/Index");
+
+            return RedirectToPage("/Account/Login", new { area = "Identity" });
         }
     }
 }
