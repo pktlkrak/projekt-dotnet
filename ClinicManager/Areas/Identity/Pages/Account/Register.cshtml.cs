893ab28d019ae653690cf3209997bf885407b8c1
pktlkrak <94856631+pktlkrak@users.noreply.github.com>
2026-06-10 04:39:23 +0200

    Move identity pages to Areas (otherwise they do not refresh) (#3)

--- ClinicManager/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ClinicManager/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,14 @@ namespace ClinicManager.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [Display(Name = "First name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last name")]
+            public string LastName { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -117,6 +125,8 @@ namespace ClinicManager.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
